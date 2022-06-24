16b23e54b0d892f2c452651ff536e9be29892134
Arturo Avila <mysticmetromx@gmail.com>
2022-06-24 15:26:41 -0500

    Added vial support for the default keymap.

---
+++ keyboards/adpenrose/kintsugi/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xF3, 0x9E, 0x37, 0x36, 0x11, 0x09, 0xF3, 0x59}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+#define VIAL_ENCODER_DEFAULT { KC_VOLD, KC_VOLU, KC_PGDOWN, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
